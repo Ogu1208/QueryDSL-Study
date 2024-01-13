743d734e7029068c0fe1003427783a16f6bda5bd
Ogu1208 <kdasunb@naver.com>
2024-01-13 23:27:39 +0900

    first commit

---
+++ src/main/java/com/example/qudrydslstudy/QudryDslStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.qudrydslstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QudryDslStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QudryDslStudyApplication.class, args);
+    }
+
+}
